fc41e8a8dbe632234c6b9f36b95a3ea1a4257c50
yduoc407-source <yduoc407@gmail.com>
2026-09-18 09:36:58 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const C="td11a9-v1";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin||e.request.method!=="GET")return;
+  e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r})
+    .catch(()=>caches.match(e.request)));
+});
